e79383137adfc94bccf914c21ddeebc542241588
YakovKunchin <yakovkunchin@gmail.com>
2021-02-08 11:41:48 +0200

    and float for task1

--- strelkina/lab1/lab1/Task1.cs
+++ strelkina/lab1/lab1/Task1.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace lab1
@@ -11,25 +7,37 @@ namespace lab1
     {
         public async Task Execute()
         {
-            var timeByRow = new List<long>();
+            var timeByRowForInt = new List<long>();
 
-            var timeByColumn = new List<long>();
+            var timeByColumnForInt = new List<long>();
+            
+            var timeByRowForFloat = new List<long>();
+            
+            var timeByColumnForFloat = new List<long>();
             
             var computer = new Computer();
             
             for (var i = 0; i <= 12_000; i += 500)
             {
-                var byRow = computer.TimeItTakes(() => { new int[i, i].FillArrayByRow(); });
-                timeByRow.Add(byRow);
+                var byRowForInt = computer.TimeItTakes(() => { new int[i, i].FillArrayByRow(); });
+                timeByRowForInt.Add(byRowForInt);
 
-                var byColumn = computer.TimeItTakes(() => { new int[i, i].FillArrayByColumn(); });
-                timeByColumn.Add(byColumn);
+                var byColumnForInt = computer.TimeItTakes(() => { new int[i, i].FillArrayByColumn(); });
+                timeByColumnForInt.Add(byColumnForInt);
+                
+                var byRowForFloat = computer.TimeItTakes(() => { new int[i, i].FillArrayByRow(); });
+                timeByRowForFloat.Add(byRowForFloat);
+
+                var byColumnForFloat = computer.TimeItTakes(() => { new int[i, i].FillArrayByColumn(); });
+                timeByColumnForFloat.Add(byColumnForFloat);
             }
             
             await new ResultWriter().Write(new List<KeyValuePair<string, List<long>>>()
             {
-                KeyValuePair.Create($"{nameof(Task1)} {nameof(timeByRow)}", timeByRow),
-                KeyValuePair.Create($"{nameof(Task1)} {nameof(timeByColumn)}", timeByColumn)
+                KeyValuePair.Create($"{nameof(Task1)} {nameof(timeByRowForInt)}", timeByRowForInt),
+                KeyValuePair.Create($"{nameof(Task1)} {nameof(timeByColumnForInt)}", timeByColumnForInt),
+                KeyValuePair.Create($"{nameof(Task1)} {nameof(timeByRowForFloat)}", timeByRowForFloat),
+                KeyValuePair.Create($"{nameof(Task1)} {nameof(timeByColumnForFloat)}", timeByColumnForFloat)
             });
         }
     }
