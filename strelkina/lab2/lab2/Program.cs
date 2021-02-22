0d83dfe644800e7f9f66386ccf9b54b1f5819fdd
YakovKunchin <yakovkunchin@gmail.com>
2021-02-22 09:31:52 +0200

    Lab 2 fixes

--- strelkina/lab2/lab2/Program.cs
+++ strelkina/lab2/lab2/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace lab2
 {
@@ -10,83 +13,43 @@ namespace lab2
         static void Main()
         {
             var computer = new Computer();
+            
+            foreach (var num in new List<int> {2, 4, 8, 10})
+            {
+                var time = computer.TimeItTakes(() =>
+                    Parallel.Invoke(new ParallelOptions {MaxDegreeOfParallelism = num},
+                        () => new FromWebToLocal().Execute())
+                );
+                Console.WriteLine($"{nameof(FromWebToLocal)}, MaxDegreeOfParallelism: {num}. Time: {time}.");
+            }
 
-            var time = computer.TimeItTakes(() => new FromWebToLocal().OneTask());
-            Console.WriteLine(time);
+            Console.WriteLine();
             
-            time = computer.TimeItTakes(() => new FromWebToLocal().TwoTasks());
-            Console.WriteLine(time);
-            
-            time = computer.TimeItTakes(() => new FromLocalToLocal().OneTask());
-            Console.WriteLine(time);
-            
-            time = computer.TimeItTakes(async () => await new FromLocalToLocal().TwoTasks());
-            Console.WriteLine(time);
-
-            time = computer.TimeItTakes(async () => await new FromLocalToLocal().ThreeTasks());
-            Console.WriteLine(time);
-            
-            time = computer.TimeItTakes(async () => await new FromLocalToLocal().FourTasks());
-            Console.WriteLine(time);
-            //await FourTasks.Execute().ConfigureAwait(false);
-            //await EightTasks.Execute().ConfigureAwait(false);
-            //await EightTasks.Execute().ConfigureAwait(false);
-            //await TenTasks.Execute().ConfigureAwait(false);
+            foreach (var num in new List<int> {2, 4, 8, 10})
+            {
+                var time = computer.TimeItTakes(() =>
+                    Parallel.Invoke(new ParallelOptions {MaxDegreeOfParallelism = num},
+                        async () => await new FromLocalToLocal().Execute())
+                );
+                Console.WriteLine($"{nameof(FromLocalToLocal)}, MaxDegreeOfParallelism: {num}. Time: {time}.");
+            }
         }
     }
 
     public class FromWebToLocal
     {
-        public void OneTask()
+        public void Execute()
         {
             var uri = new Uri(@"https://javaconceptoftheday.com/wp-content/uploads/2016/08/NumberPatternPrograms.png");
             
             using var client = new WebClient();
-            client.DownloadFile(uri, $"{nameof(FromWebToLocal)}. NumberPatternPrograms.png");
-        }
-        
-        public void TwoTasks()
-        {
-            var uri = new Uri(@"https://javaconceptoftheday.com/wp-content/uploads/2016/08/NumberPatternPrograms.png");
-            
-            using var client = new WebClient();
-            client.DownloadFileAsync(uri, $"{nameof(TwoTasks)}. NumberPatternPrograms.png");
+            client.DownloadFileAsync(uri, $"{nameof(Execute)}. NumberPatternPrograms.png");
         }
     }
     
     public class FromLocalToLocal
     {
-        public void OneTask()
-        {
-            using var sr = new StreamReader("NumberPatternPrograms.png");
-            var file = sr.ReadToEnd();
-
-            Directory.CreateDirectory("Rewritten");
-            using var sw = new StreamWriter("Rewritten/NumberPatternPrograms.png");
-            sw.Write(file);
-        }
-        
-        public async Task TwoTasks()
-        {
-            using var sr = new StreamReader("NumberPatternPrograms.png");
-            var file = await sr.ReadToEndAsync().ConfigureAwait(false);
-
-            Directory.CreateDirectory("Rewritten");
-            using var sw = new StreamWriter("Rewritten/NumberPatternPrograms.png");
-            sw.Write(file);
-        }
-        
-        public async Task ThreeTasks()
-        {
-            using var sr = new StreamReader("NumberPatternPrograms.png");
-            var file = await sr.ReadToEndAsync().ConfigureAwait(false);
-
-            Directory.CreateDirectory("Rewritten");
-            await using var sw = new StreamWriter("Rewritten/NumberPatternPrograms.png");
-            sw.WriteAsync(file);
-        }
-        
-        public async Task FourTasks()
+        public async Task Execute()
         {
             using var sr = new StreamReader("NumberPatternPrograms.png");
             var file = await sr.ReadToEndAsync().ConfigureAwait(false);
