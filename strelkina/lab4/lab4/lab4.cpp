8864f53749e5ea5c7e4aa193b1d422c9aa508426
YakovKunchin <yakovkunchin@gmail.com>
2021-04-17 14:21:41 +0300

    lab 4

--- strelkina/lab4/lab4/lab4.cpp
+++ strelkina/lab4/lab4/lab4.cpp
@@ -10,7 +10,7 @@ void randomize_matrix(int** matrix, int n, int m) {
 			matrix[i][j] = rand() % 11;
 		}
 	}
-	return;
+
 }
 
 int** create_matrix(int n1, int m1)
@@ -22,40 +22,47 @@ int** create_matrix(int n1, int m1)
 	return matrix;
 }
 
-int main() {
-	srand(time(nullptr));
-	int n1 = 1000;
-	int m1 = 500;
-	int n2 = 500;
-	int m2 = 1200;
+void do_work(const int size, const int pipe)
+{
+	int** matrix1 = create_matrix(size, size);
+	int** matrix2 = create_matrix(size, size);
 
-	int** matrix1 = create_matrix(n1, m1);
-	int** matrix2 = create_matrix(n2, m2);
+	randomize_matrix(matrix1, size, size);
+	randomize_matrix(matrix2, size, size);
 
-	//Генерируем случайные матрицы для умножения
-	randomize_matrix(matrix1, n1, m1);
-	randomize_matrix(matrix2, n2, m2);
+	int** result = create_matrix(size, size);
 
-	int** result = create_matrix(n1, m2);
-
-	//Устанавливаем число потоков
-	int threadsNum = 2;
 	omp_set_dynamic(0);
-	omp_set_num_threads(threadsNum);
+	omp_set_num_threads(pipe);
 	clock_t begin = clock();
 #pragma omp parallel for shared(matrix1, matrix2, result) private(i, j, k)
 
-	for (int i = 0; i < n1; i++)
-		for (int j = 0; j < m2; j++)
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
 		{
 			result[i][j] = 0;
-			for (int k = 0; k < m1; k++)
-				result[i][j] += (matrix1[i][k] * matrix2[k][j]);
+			for (int k = 0; k < size; k++)
+				result[i][j] += matrix1[i][k] * matrix2[k][j];
 		}
 
-
 	clock_t end = clock();
-	double exectionTime = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
-	cout << exectionTime << endl;
+	double executionTime = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
+	cout <<"size: " << size << "x" << size << " pipes: " << pipe << " time: " << executionTime << endl;
+}
+
+int main() {
+	srand(time(nullptr));
+	
+	const int sizes[5] = {100, 300, 600, 900, 1200};
+	const int pipes[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+	for (int size : sizes)
+	{
+		for (int pipe : pipes)
+		{
+			do_work(size, pipe);
+		}
+	}
+
 	return 0;
 }
